6ef640aa3fe0219b28c29865c46cb582b22c1e19
driden <driden_87@hotmail.com>
2021-08-23 11:27:02 -0300

    emacs config cleanup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -54,7 +54,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(require 'lsp-java)
-(add-hook 'java-mode-hook #'lsp)
-
 (use-package counsel)
