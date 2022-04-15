c9f43036c2d33fdee00f8096ab32ca0f4e2cf689
driden <driden@dridens-MacBook-Pro.local>
2022-04-14 23:20:08 -0300

    Updates

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,73 +1,73 @@
 local M = {}
 
 local function get_lua_opts()
-	return {
-		settings = {
-			Lua = {
-				runtime = {
-					version = "LuaJIT",
-					path = vim.split(package.path, ";"),
-				},
-				diagnostics = { globals = { "vim" } },
-				workspace = {
-					library = vim.api.nvim_get_runtime_file("", true),
-					checkThirdParty = false,
-				},
-				telemetry = { enable = false },
-			},
-		},
-	}
+  return {
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+          path = vim.split(package.path, ";"),
+        },
+        diagnostics = { globals = { "vim", "hs" } },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        telemetry = { enable = false },
+      },
+    },
+  }
 end
 
 local function setup_server(server)
-	local opts = {}
-	if server.name == "sumneko_lua" then
-		opts = vim.tbl_deep_extend("force", get_lua_opts(), opts)
-	end
-	server:setup(opts)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = vim.tbl_deep_extend("force", get_lua_opts(), opts)
+  end
+  server:setup(opts)
 end
 
 function M.setup()
-	vim.o.completeopt = "menu,menuone,noselect"
-	local installer = require("nvim-lsp-installer")
-	installer.on_server_ready(setup_server)
-	-- This setup() function will take the provided server configuration and decorate it with the necessary properties
-	-- before passing it onwards to lspconfig.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  vim.o.completeopt = "menu,menuone,noselect"
+  local installer = require("nvim-lsp-installer")
+  installer.on_server_ready(setup_server)
+  -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+  -- before passing it onwards to lspconfig.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
-	-- nullls
-	local null_ls = require("null-ls")
-	local formatting = null_ls.builtins.formatting
-	null_ls.setup({
-		sources = {
-			formatting.clang_format,
-			formatting.cmake_format,
-			formatting.dart_format,
-			formatting.gofmt,
-			--formatting.prettier,
-			formatting.shfmt,
-			formatting.stylua.with({ indent_type = "Spaces" }),
-			null_ls.builtins.completion.spell,
-			null_ls.builtins.diagnostics.eslint,
-		},
-		on_attach = function(client)
-			if client.resolved_capabilities.document_formatting then
-				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
-				if client.resolved_capabilities.document_highlight then
-					vim.api.nvim_exec(
-						[[
+  -- nullls
+  local null_ls = require("null-ls")
+  local formatting = null_ls.builtins.formatting
+  null_ls.setup({
+    sources = {
+      formatting.clang_format,
+      formatting.cmake_format,
+      formatting.dart_format,
+      formatting.gofmt,
+      --formatting.prettier,
+      formatting.shfmt,
+      formatting.stylua.with({ indent_type = "Spaces" }),
+      null_ls.builtins.completion.spell,
+      null_ls.builtins.diagnostics.eslint,
+    },
+    on_attach = function(client)
+      if client.resolved_capabilities.document_formatting then
+        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
+        if client.resolved_capabilities.document_highlight then
+          vim.api.nvim_exec(
+            [[
           augroup document_highlight
             autocmd! * <buffer>
             autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
             autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
           augroup END
-    ]],
-						false
-					)
-				end
-			end
-		end,
-	})
+    ]]       ,
+            false
+          )
+        end
+      end
+    end,
+  })
 end
 
 return M
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,54 +2,48 @@
 -- Hammerspoon needs to be allowed in
 -- Privacy > Accessibility
 
-local hyper = {"cmd", "alt", "ctrl"}
-local hypershift = {"cmd", "alt", "ctrl", "shift"}
+local hyper = { "cmd", "alt", "ctrl" }
+local hypershift = { "cmd", "alt", "ctrl", "shift" }
 
-function h_bind(key, func)
-    hs.hotkey.bind(hyper, key, func)
+local function h_bind(key, func)
+  hs.hotkey.bind(hyper, key, func)
 end
 
-function hs_bind(key, func)
-    hs.hotkey.bind(hypershift, key, func)
+local function hs_bind(key, func)
+  hs.hotkey.bind(hypershift, key, func)
 end
 
 h_bind("r", hs.reload)
 hs_bind("r", hs.toggleConsole)
 
 
-function file_exists(path)
-    local f=io.open(path,"r")
-    if f~=nil then io.close(f) return true else return false end
-    -- ~= is != in other languages
-end
-
 -- window functions
-function positionWindow(x, y, w, h)
-    return function()
-        local win = hs.window.focusedWindow()
-        if win == nil then return end
-        local f = win:frame()
-        local s = win:screen():frame()
-        f.x = s.x + s.w * x
-        f.y = s.y + s.h * y
-        f.w = s.w * w
-        f.h = s.h * h
-        win:setFrame(f)
-    end
+local function positionWindow(x, y, w, h)
+  return function()
+    local win = hs.window.focusedWindow()
+    if win == nil then return end
+    local f = win:frame()
+    local s = win:screen():frame()
+    f.x = s.x + s.w * x
+    f.y = s.y + s.h * y
+    f.w = s.w * w
+    f.h = s.h * h
+    win:setFrame(f)
+  end
 end
 
 hs.window.animationDuration = 0
 
-h_bind("1", positionWindow(0, 0, 1/2, 1))
-h_bind("2", positionWindow(1/2, 0, 1/2, 1))
+h_bind("1", positionWindow(0, 0, 1 / 2, 1))
+h_bind("2", positionWindow(1 / 2, 0, 1 / 2, 1))
 
-h_bind("u", positionWindow(0, 0, 1/2, 1/2))
-h_bind("i", positionWindow(1/2, 0, 1/2, 1/2))
-h_bind("j", positionWindow(0, 1/2, 1/2, 1/2))
-h_bind("k", positionWindow(1/2, 1/2, 1/2, 1/2))
+h_bind("u", positionWindow(0, 0, 1 / 2, 1 / 2))
+h_bind("i", positionWindow(1 / 2, 0, 1 / 2, 1 / 2))
+h_bind("j", positionWindow(0, 1 / 2, 1 / 2, 1 / 2))
+h_bind("k", positionWindow(1 / 2, 1 / 2, 1 / 2, 1 / 2))
 
-h_bind("q", positionWindow(0, 0, 2/3, 1))
-h_bind("w", positionWindow(2/3, 0, 1/3, 1))
+h_bind("q", positionWindow(0, 0, 2 / 3, 1))
+h_bind("w", positionWindow(2 / 3, 0, 1 / 3, 1))
 
 -- grid based window functions
 -- cols, rows
@@ -57,67 +51,67 @@ hs.grid.setGrid('10x8')
 hs.grid.setMargins('0x0')
 
 h_bind("right", function()
-    local win = hs.window.focusedWindow()
-    if win == nil then return end
-    local screen = win:screen()
-    local sg = hs.grid.getGrid(screen)
-    local g = hs.grid.get(win)
-    if g.x + g.w == sg.w then
-        g.x = g.x + 1
-        g.w = g.w - 1
-        hs.grid.set(win, g)
-    else
-        g.w = g.w + 1
-        hs.grid.set(win, g)
-    end
+  local win = hs.window.focusedWindow()
+  if win == nil then return end
+  local screen = win:screen()
+  local sg = hs.grid.getGrid(screen)
+  local g = hs.grid.get(win)
+  if g.x + g.w == sg.w then
+    g.x = g.x + 1
+    g.w = g.w - 1
+    hs.grid.set(win, g)
+  else
+    g.w = g.w + 1
+    hs.grid.set(win, g)
+  end
 end)
 
 h_bind("left", function()
-    local win = hs.window.focusedWindow()
-    if win == nil then return end
-    local screen = win:screen()
-    local sg = hs.grid.getGrid(screen)
-    local g = hs.grid.get(win)
-    if g.x + g.w >= sg.w and g.x ~= 0 then
-        g.x = g.x - 1
-        g.w = g.w + 1
-        hs.grid.set(win, g)
-    else
-        g.w = g.w - 1
-        hs.grid.set(win, g)
-    end
+  local win = hs.window.focusedWindow()
+  if win == nil then return end
+  local screen = win:screen()
+  local sg = hs.grid.getGrid(screen)
+  local g = hs.grid.get(win)
+  if g.x + g.w >= sg.w and g.x ~= 0 then
+    g.x = g.x - 1
+    g.w = g.w + 1
+    hs.grid.set(win, g)
+  else
+    g.w = g.w - 1
+    hs.grid.set(win, g)
+  end
 end)
 
 h_bind("down", function()
-    local win = hs.window.focusedWindow()
-    if win == nil then return end
-    local screen = win:screen()
-    local sg = hs.grid.getGrid(screen)
-    local g = hs.grid.get(win)
-    if g.y + g.h == sg.h then
-        g.y = g.y + 1
-        g.h = g.h - 1
-        hs.grid.set(win, g)
-    else
-        g.h = g.h + 1
-        hs.grid.set(win, g)
-    end
+  local win = hs.window.focusedWindow()
+  if win == nil then return end
+  local screen = win:screen()
+  local sg = hs.grid.getGrid(screen)
+  local g = hs.grid.get(win)
+  if g.y + g.h == sg.h then
+    g.y = g.y + 1
+    g.h = g.h - 1
+    hs.grid.set(win, g)
+  else
+    g.h = g.h + 1
+    hs.grid.set(win, g)
+  end
 end)
 
 h_bind("up", function()
-    local win = hs.window.focusedWindow()
-    if win == nil then return end
-    local screen = win:screen()
-    local sg = hs.grid.getGrid(screen)
-    local g = hs.grid.get(win)
-    if g.y + g.h >= sg.h and g.y ~= 0 then
-        g.y = g.y - 1
-        g.h = g.h + 1
-        hs.grid.set(win, g)
-    else
-        g.h = g.h - 1
-        hs.grid.set(win, g)
-    end
+  local win = hs.window.focusedWindow()
+  if win == nil then return end
+  local screen = win:screen()
+  local sg = hs.grid.getGrid(screen)
+  local g = hs.grid.get(win)
+  if g.y + g.h >= sg.h and g.y ~= 0 then
+    g.y = g.y - 1
+    g.h = g.h + 1
+    hs.grid.set(win, g)
+  else
+    g.h = g.h - 1
+    hs.grid.set(win, g)
+  end
 end)
 
 
@@ -127,41 +121,41 @@ hs_bind("down", hs.grid.pushWindowDown)
 hs_bind("up", hs.grid.pushWindowUp)
 
 h_bind('n', function()
-    -- get the focused window
-    local win = hs.window.focusedWindow()
-    local isFullScreen = win:isFullScreen()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  local isFullScreen = win:isFullScreen()
 
-    if isFullScreen then
-        win:setFullScreen(false)
-    end
+  if isFullScreen then
+    win:setFullScreen(false)
+  end
 
-    --
-    -- get the screen where the focused window is displayed, a.k.a. current screen
-    local screen = win:screen()
-    win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+  --
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
 
-    if isFullScreen then
-        hs.timer.doAfter(0.6, function()
-        win:setFullScreen(true)
-        end)
-    end
+  if isFullScreen then
+    hs.timer.doAfter(0.6, function()
+      win:setFullScreen(true)
+    end)
+  end
 end)
 
 h_bind('f', function()
-    local win = hs.window.focusedWindow()
-    win:toggleFullscreen()
+  local win = hs.window.focusedWindow()
+  win:toggleFullscreen()
 end)
 
 h_bind("m", function()
-    local currentAudio = hs.audiodevice.current(true).device
+  local currentAudio = hs.audiodevice.current(true).device
 
-    if currentAudio:muted() then
-        currentAudio:setMuted(false)
-        hs.alert.show("unmuted mic")
-    else
-        currentAudio:setMuted(true)
-        hs.alert.show("muted mic")
-    end
+  if currentAudio:muted() then
+    currentAudio:setMuted(false)
+    hs.alert.show("unmuted mic")
+  else
+    currentAudio:setMuted(true)
+    hs.alert.show("muted mic")
+  end
 end)
 
 hs.alert.show("Hammerspoon started")
