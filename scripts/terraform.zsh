0a1994a8c084d9fa32dadef9fea3ebd4131d97c8
driden <driden_87@hotmail.com>
2022-05-09 10:22:57 -0300

    bleh

--- scripts/terraform.zsh
+++ scripts/terraform.zsh
@@ -1,10 +1,13 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env zsh -e
 
-function tws(){
+function tws() {
   local list=$(terraform workspace list)
   local current=$(echo "$list" | awk '$1 == "*" {print $2}')
   local ws=$(echo "$list" | awk '$1 != "*" {print $1}')
   local header=$(printf "Using:%s\nChoose new" "${current}")
   local workspace=$(echo "$ws" | fzf --border --margin=1 --layout=reverse --padding=1 --header "$header")
+  
+  [[ -z $workpace ]] && return 1
+
   terraform workspace select "${workspace}"
 }
