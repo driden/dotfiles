a4e3873a53e72b418a92cf4af8be7a36572110f1
driden <driden_87@hotmail.com>
2022-05-23 13:10:23 -0300

    +jdtls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,5 @@
+-- If you are looking for java, then its at ../../ftplugin/
 local M = {}
-
-local function get_jdtls_opts()
-  return {
-    settings = {},
-  }
-end
 
 local function get_lua_opts()
   return {
@@ -56,22 +51,55 @@ function M.setup()
       null_ls.builtins.completion.spell,
       null_ls.builtins.diagnostics.eslint,
     },
-    on_attach = function(client)
+    on_attach = function(client, bufnr)
       if client.resolved_capabilities.document_formatting then
         vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
         if client.resolved_capabilities.document_highlight then
+          -- TODO: Neovim has a new api for autocommands, check that out
           vim.api.nvim_exec(
             [[
           augroup document_highlight
             autocmd! * <buffer>
             autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
             autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-          augroup END
-    ]]       ,
+            augroup END
+          ]] ,
             false
           )
         end
       end
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(
+        bufnr,
+        "n",
+        "<space>wa",
+        "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",
+        opts
+      )
+      vim.api.nvim_buf_set_keymap(
+        bufnr,
+        "n",
+        "<space>wr",
+        "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",
+        opts
+      )
+      vim.api.nvim_buf_set_keymap(
+        bufnr,
+        "n",
+        "<space>wl",
+        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+        opts
+      )
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     end,
   })
 end
