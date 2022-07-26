f62c6db1f1bbc44dbd496ba9becb971e016cb8a7
Loureiro de Rezendes <lrrezend@147dda3ab616.ant.amazon.com>
2022-07-26 17:09:01 -0300

    back to doom modeline!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -365,26 +365,7 @@
 (setq ddn/current-theme 'doom-old-hope)
 (ddn/set-theme 'doom-old-hope)
 
-
-(use-package telephone-line
-	:init
-		(setq telephone-line-lhs
-				'((evil   . (telephone-line-evil-tag-segment))
-					(accent . (telephone-line-vc-segment
-										telephone-line-erc-modified-channels-segment
-										telephone-line-process-segment))
-				  (nil    . (telephone-line-minor-mode-segment
-										telephone-line-buffer-segment))))
-		(setq telephone-line-rhs
-				'((nil    . (telephone-line-misc-info-segment))
-					(accent . (telephone-line-major-mode-segment))
-					(evil   . (telephone-line-airline-position-segment))))
-	:config
-	  (telephone-line-mode 1))
-(use-package moody
-  :config
-  (setq x-underline-at-descent-line t)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode)
-  (moody-replace-eldoc-minibuffer-message-function))
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
     
