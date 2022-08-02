63547343fb6aef8dd7e9c19223190711b03764a4
Loureiro de Rezendes <lrrezend@147dda3ab616.ant.amazon.com>
2022-08-02 12:36:54 -0300

    themes

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -2,18 +2,10 @@ local M = {}
 M.available_themes = {
   { id = 1, name = "palenight", plugin = "drewtempelmeyer/palenight.vim" },
   { id = 2, name = "onedark", plugin = "joshdick/onedark.vim" },
-  { id = 3, name = "gruvbox", plugin = "sainnhe/gruvbox-material" },
+  { id = 3, name = "gruvbox-material", plugin = "sainnhe/gruvbox-material", bar = "gruvbox-material" },
   { id = 4, name = "jellybeans", plugin = "metalelf0/jellybeans-nvim" },
   { id = 5, name = "PaperColor", bar = "papercolor", plugin = "papercolor-theme" },
 }
-
-local function find_theme_by_id(id)
-  for _, v in ipairs(M.available_themes) do
-    if v.id == id then
-      return v
-    end
-  end
-end
 
 local function find_theme_by_name(name)
   for _, v in ipairs(M.available_themes) do
@@ -23,13 +15,6 @@ local function find_theme_by_name(name)
   end
 end
 
-local function set_colorscheme_by_id(id)
-  local theme = find_theme_by_id(id)
-
-  vim.cmd("colorscheme " .. theme.name)
-  vim.cmd("let g:airline_theme='" .. theme.bar .. "'")
-end
-
 local function set_colorscheme_by_name(name)
   local theme = find_theme_by_name(name)
 
@@ -37,10 +22,10 @@ local function set_colorscheme_by_name(name)
   vim.cmd("let g:airline_theme='" .. theme.bar .. "'")
 end
 
-M.theme_id = 5
+M.theme_name = "gruvbox-material"
 
 function M.load_theme()
-  set_colorscheme_by_id(5)
+  set_colorscheme_by_name("gruvbox-material")
 
   -- Nicer symbols!
   local airline_symbols = {
