853dee349d03754fd5191d3d01fe056171d7c628
Loureiro de Rezendes <lrrezend@147dda3ab616.ant.amazon.com>
2022-08-19 14:55:57 -0300

    stuff not really working

--- .config/nvim/lua/plugins/plugin_manager.lua
+++ .config/nvim/lua/plugins/plugin_manager.lua
@@ -1,55 +1,57 @@
 local M = {}
 
 function M.setup()
-  local paq = require "paq"
+  local paq = require("paq")
   local plugins = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
     -- THEMES
-    'drewtempelmeyer/palenight.vim',
-    'editorconfig/editorconfig-vim',
-    'joshdick/onedark.vim',
-    'sainnhe/gruvbox-material',
-    'NLKNguyen/papercolor-theme',
-    'metalelf0/jellybeans-nvim',
-    'rktjmp/lush.nvim',
+    "drewtempelmeyer/palenight.vim",
+    "editorconfig/editorconfig-vim",
+    "joshdick/onedark.vim",
+    "sainnhe/gruvbox-material",
+    "NLKNguyen/papercolor-theme",
+    "metalelf0/jellybeans-nvim",
+    "rktjmp/lush.nvim",
 
     -- fzf
-    'junegunn/fzf', -- { 'do',: { -> fzf#install() } }
-    'junegunn/fzf.vim',
+    "junegunn/fzf", -- { 'do',: { -> fzf#install() } }
+    "junegunn/fzf.vim",
     -- git
-    'tpope/vim-fugitive',
-    'tpope/vim-surround',
+    "tpope/vim-fugitive",
+    "tpope/vim-surround",
     -- statusbar
-    'vim-airline/vim-airline',
-    'vim-airline/vim-airline-themes',
+    "vim-airline/vim-airline",
+    "vim-airline/vim-airline-themes",
     --term
-    'akinsho/toggleterm.nvim',
+    "akinsho/toggleterm.nvim",
 
     -- filetree
-    'kyazdani42/nvim-web-devicons',
-    'kyazdani42/nvim-tree.lua',
+    "kyazdani42/nvim-web-devicons",
+    "kyazdani42/nvim-tree.lua",
     -- Treesitter
-    'nvim-treesitter/nvim-treesitter', --{do = 'TSUpdate',}},
+    "nvim-treesitter/nvim-treesitter", --{do = 'TSUpdate',}},
 
     -- LSP
-    'neovim/nvim-lspconfig',
-    'onsails/lspkind-nvim',
-    'williamboman/nvim-lsp-installer',
-    'jose-elias-alvarez/null-ls.nvim',
-    'mfussenegger/nvim-jdtls',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-nvim-lsp-document-symbol',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip'
+    "neovim/nvim-lspconfig",
+    "onsails/lspkind-nvim",
+    "williamboman/nvim-lsp-installer",
+    "jose-elias-alvarez/null-ls.nvim",
+    "mfussenegger/nvim-jdtls",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    -- DAP
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
   }
-
 
   paq(plugins)
 end
