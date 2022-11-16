93b04e8a0a018ba87168341cc3203f135c6e2f9d
Ignacio Loureiro <lrrezend@amazon.com>
2022-11-16 15:34:02 -0300

    bleh

--- .config/nvim/lua/plugins/lsp/config.lua
+++ .config/nvim/lua/plugins/lsp/config.lua
@@ -4,24 +4,25 @@
 -- 1) create file langs/<lang>.lua
 -- 2) write setup function and register it to the servers list
 -- 3) require it here
--- TODO:: 
+-- TODO::
 --  List all the files in the langs subfolder and just require everything automatically
-require("plugins.lsp.langs.sumneko")
 require("plugins.lsp.langs.barium")
+require("plugins.lsp.langs.bashls")
+require("plugins.lsp.langs.sumneko")
 require("plugins.lsp.langs.ts")
 
 local names = {}
 local fs = {}
 for _, v in ipairs(require("plugins.lsp.servers").servers) do
-  local val = vim.tbl_values(v)
-  table.insert(names, val[1])
-  table.insert(fs, val[2])
+	local val = vim.tbl_values(v)
+	table.insert(names, val[1])
+	table.insert(fs, val[2])
 end
 
 require("mason-lspconfig").setup({
-	ensure_installed = names
+	ensure_installed = names,
 })
 
 for _, setup in ipairs(fs) do
-  setup()
+	setup()
 end
