04c34a4bee706a90209199028a32a988a0399c16
driden <driden@192.168.1.6>
2022-11-21 19:33:36 -0300

    bleh

--- scripts/rkt.sh
+++ scripts/rkt.sh
@@ -4,8 +4,7 @@ rkt () {
   kitty_dir="$HOME/.config/kitty"
   kitty_themes_dir="$kitty_dir/kitty-themes/themes"
 
-  new_theme=$(ls -al $kitty_themes_dir |  awk '{print $9}' | xargs shuf -n1 -e --)
-  echo $new_theme
+  new_theme="$(ls -al $kitty_themes_dir |  awk '{print $9}' | xargs shuf -n1 -e --)"
   rm -f "$kitty_dir/theme.conf" &&
   ln -s "$kitty_themes_dir/$new_theme" "$kitty_dir/theme.conf" &&
   echo "using theme $new_theme"
