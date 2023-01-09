ae9399ae0e65b4db3f2727d19755895709de72fd
Ignacio Loureiro <lrrezend@amazon.com>
2023-01-09 10:20:40 -0300

    updated nvim

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,15 +1,9 @@
-local M = {}
-
-function M.setup()
-  require("nvim-treesitter.configs").setup({
-    -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-    highlight = {
-      enable = true,
-      disable = { "org" }, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-      additional_vim_regex_highlighting = { "org" }, -- Required since TS highlighter doesn't support all syntax features (conceal)
-    },
-    ensure_installed = { "org", "hcl", "bash", "typescript" },
-  })
-end
-
-return M
+require("nvim-treesitter.configs").setup({
+	-- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+	highlight = {
+		enable = true,
+		disable = { "org" }, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+		additional_vim_regex_highlighting = { "org" }, -- Required since TS highlighter doesn't support all syntax features (conceal)
+	},
+	ensure_installed = { "org", "hcl", "bash", "typescript" },
+})
