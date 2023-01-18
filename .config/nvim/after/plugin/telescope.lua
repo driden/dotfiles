e0d0c7d92f37ff5a4d50d3cc0b605fe72508e932
driden <driden@192.168.1.10>
2023-01-17 22:06:16 -0300

    telescopic changes

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,18 @@
-require("telescope").setup {
-  defaults = {
-    file_ignore_patterns = {"vendor", "node_modules"},
-    file_sorter = require("telescope.sorters").get_fzy_sorter,
-    color_devicons = true,
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-  },
-  pickers = {}
-}
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "vendor", "node_modules" },
+		file_sorter = require("telescope.sorters").get_fzy_sorter,
+		color_devicons = true,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+	},
+	pickers = {},
+})
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>bD",
+	":lua require('driden.telescope').del_buffers()<cr>",
+	{ noremap = true, silent = false }
+)
