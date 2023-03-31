3d96b3ab1607827c89b4fa8c0e47a72c638cc84f
Ignacio Loureiro <lrrezend@amazon.com>
2023-03-31 13:21:30 -0300

    javita todo rotito

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -2,47 +2,62 @@ local status, jdtls = pcall(require, "jdtls")
 if not status then
 	return
 end
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- local extendedClientCapabilities = require("jdtls").extendedClientCapabilities
+-- extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+--
+local capabilities = {
+	workspace = {
+		configuration = true,
+	},
+	textDocument = {
+		completion = {
+			completionItem = {
+				snippetSupport = true,
+			},
+		},
+	},
+}
 local extendedClientCapabilities = require("jdtls").extendedClientCapabilities
 extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
-local ws_folders_jdtls = {}
 
-local root_markers = { "packageInfo" }
--- local root_markers = {".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = require("jdtls.setup").find_root(root_markers, "Config")
+local root_markers = { ".git", "gradlew", "pom.xml" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
 local home = os.getenv("HOME")
 
-if root_dir then -- root workspace
-	local file = io.open(root_dir .. "/.bemol/ws_root_folders")
-	if file then
-		for line in file:lines() do
-			table.insert(ws_folders_jdtls, "file://" .. line)
-		end
-		file:close()
-	end
-else
-	return
-end
-
--- local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local eclipse_workspace = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
---local workspace_dir = WORKSPACE_PATH .. project_name
-local workspace_dir = eclipse_workspace
-
+local workspace_dir = home .. "/.local/workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local java_folder = "$HOME/.sdkman/candidates/java/17.0.5-tem"
+local java = java_folder .. "/bin/java"
+local on_attach = require("lsp.keymaps").on_attach
+
+local server_installation = vim.fn.stdpath("data") .. "/mason/packages/jdtls/plugins"
+
+local launcher = vim.fn.glob(server_installation .. "/org.eclipse.equinox.launcher_*.jar")
 local config = {
 	cmd = {
-		"jdtls",
+		java,
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx4g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
 		"--jvm-arg=-javaagent:" .. home .. "/.local/share/nvim/lsp_servers/jdtls/lombok.jar",
+		"-jar",
+		launcher,
 		"-data",
 		workspace_dir,
 	},
-	on_attach = require("plugins.lsp.keymaps").on_attach,
+	on_attach = on_attach,
 	capabilities = capabilities,
 	root_dir = root_dir,
 	init_options = {
-		workspaceFolders = ws_folders_jdtls,
 		extendedClientCapabilities = extendedClientCapabilities,
 	},
 	format = {
@@ -75,16 +90,27 @@ local config = {
 	},
 }
 
-config.on_init =
-	function(client, _)
-		client.notify("workspace/didChangeConfiguration", { settings = config.settings })
-	end,
-		-- This starts a new client & server,
-	-- or attaches to an existing client & server depending on the `root_dir`.
-	--
+config.on_init = function(client, _)
+	client.notify("workspace/didChangeConfiguration", { settings = config.settings })
+end
+
+vim.pretty_print(config.cmd)
 require("jdtls").start_or_attach(config)
 
--- require('jdtls').setup_dap()
+local list = require("lsp.servers").servers
+
+local setup = function()
+	local config = require("lspconfig")
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	local on_attach = require("lsp.keymaps").on_attach
+
+	config.jdtls.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+end
+
+table.insert(list, { "bashls", setup })
 
 vim.cmd(
 	"command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
