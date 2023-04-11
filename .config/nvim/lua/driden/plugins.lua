733e441be4321de26ab873cff5ef2424e484cce8
Ignacio Loureiro <lrrezend@amazon.com>
2023-04-11 15:20:34 -0300

    update

--- .config/nvim/lua/driden/plugins.lua
+++ .config/nvim/lua/driden/plugins.lua
@@ -3,118 +3,120 @@
 -- https://github.com/folke/noice.nvim
 -- https://github.com/folke/lazy.nvim
 -- https://github.com/echasnovski/mini.nvim
+
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    Packer_bootstrap = fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    })
-    vim.cmd([[packadd packer.nvim]])
+  Packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  vim.cmd([[packadd packer.nvim]])
 end
 
 require("packer").startup(function(use)
-    local plugins = {
-        "AndrewRadev/bufferize.vim",
-        "wbthomason/packer.nvim",
-        "nvim-lua/plenary.nvim",
-        -- THEMES
-        "folke/tokyonight.nvim",
-        "drewtempelmeyer/palenight.vim",
-        "editorconfig/editorconfig-vim",
-        "joshdick/onedark.vim",
-        "sainnhe/gruvbox-material",
-        "NLKNguyen/papercolor-theme",
-        "rktjmp/lush.nvim",
-        "rebelot/kanagawa.nvim",
+  local plugins = {
+    "AndrewRadev/bufferize.vim",
+    "wbthomason/packer.nvim",
+    "nvim-lua/plenary.nvim",
+    -- THEMES
+    "folke/tokyonight.nvim",
+    "drewtempelmeyer/palenight.vim",
+    "editorconfig/editorconfig-vim",
+    "joshdick/onedark.vim",
+    "sainnhe/gruvbox-material",
+    "NLKNguyen/papercolor-theme",
+    "rktjmp/lush.nvim",
+    "rebelot/kanagawa.nvim",
 
-        "tpope/vim-surround",
+    "tpope/vim-surround",
 
-        -- statusbar
-        "vim-airline/vim-airline",
-        "vim-airline/vim-airline-themes",
+    -- statusbar
+    "vim-airline/vim-airline",
+    "vim-airline/vim-airline-themes",
 
-        -- filetree
-        "kyazdani42/nvim-web-devicons",
-        "kyazdani42/nvim-tree.lua",
-        -- Treesitter
-        "nvim-treesitter/nvim-treesitter", --{do = 'TSUpdate',}},
+    -- filetree
+    "kyazdani42/nvim-web-devicons",
+    "kyazdani42/nvim-tree.lua",
+    -- Treesitter
+    "nvim-treesitter/nvim-treesitter", --{do = 'TSUpdate',}},
 
-        -- Indent
-        "lukas-reineke/indent-blankline.nvim",
+    -- Indent
+    "lukas-reineke/indent-blankline.nvim",
 
-        -- LSP
-        "folke/neodev.nvim",
-        "neovim/nvim-lspconfig",
-        "onsails/lspkind-nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "jose-elias-alvarez/null-ls.nvim",
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-omni",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-        "hrsh7th/cmp-nvim-lsp-document-symbol",
-        "hrsh7th/cmp-buffer",
-        --
-        --Java
-        "mfussenegger/nvim-jdtls",
+    -- LSP
+    "folke/neodev.nvim",
+    "neovim/nvim-lspconfig",
+    "onsails/lspkind-nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-omni",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-buffer",
+    --
+    --Java
+    "mfussenegger/nvim-jdtls",
+    "mfussenegger/nvim-dap",
 
-        -- Snippets
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+    -- Snippets
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
 
-        "numToStr/Comment.nvim",
+    "numToStr/Comment.nvim",
 
-        -- boludeces
-        "xiyaowong/nvim-transparent",
-    }
+    -- boludeces
+    "xiyaowong/nvim-transparent",
+  }
 
-    for _, plugin in ipairs(plugins) do
-        use(plugin)
-    end
+  for _, plugin in ipairs(plugins) do
+    use(plugin)
+  end
 
-    use({
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    })
-    use({
-        "phaazon/hop.nvim",
-        branch = "v2", -- optional but strongly recommended
-        config = function()
-            -- you can configure Hop the way you like here; see :h hop-config
-            require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-        end,
-    })
-    -- Lua
-    use({
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-    })
-    use({
-        "nvim-neotest/neotest",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim",
-            "marilari88/neotest-vitest",
-        },
-    })
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  use({
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+    end,
+  })
+  -- Lua
+  use({
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+  })
+  use({
+    "nvim-neotest/neotest",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "marilari88/neotest-vitest",
+    },
+  })
 
-    use({
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons", opt = true },
-    })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
 
-    if Packer_bootstrap then
-        require("packer").sync()
-    end
+  if Packer_bootstrap then
+    require("packer").sync()
+  end
 end)
