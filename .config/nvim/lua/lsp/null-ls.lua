7ae54d0e8cf079af17b11f3572918bc2026f2c2a
driden <driden@192.168.1.9>
2023-10-02 17:32:52 -0300

    wip

--- .config/nvim/lua/lsp/null-ls.lua
+++ .config/nvim/lua/lsp/null-ls.lua
@@ -11,22 +11,22 @@ local with_root_file = function(...)
 end
 
 -- https://github.com/0x221A/dotfiles/blob/2a1cc44bf39268222377d8c6244c9535fd493b83/packages/nvim/nvim/lua/rootblack45/configs/lsp/null-ls.lua#L18
-local project_node_bin = 'node_modules/.bin'
-local local_eslint = project_node_bin .. '/eslint'
+local project_node_bin = "node_modules/.bin"
+local local_eslint = project_node_bin .. "/eslint"
 
 require("null-ls").setup({
-    debug = false, -- :NullLsLog && :NullLsInfo
-    sources = {
-        code_actions.eslint_d,
-        diagnostics.eslint_d.with({
-            condition = with_root_file(local_eslint)
-        }),
-        formatting.eslint_d.with({
-            condition = with_root_file(local_eslint)
-        }),
-        formatting.shfmt,
-        formatting.stylua,
-        --formatting.google_java_format
-    },
-    on_attach = require("lsp.keymaps").on_attach,
+  debug = false,   -- :NullLsLog && :NullLsInfo
+  sources = {
+    code_actions.eslint_d,
+    diagnostics.eslint_d.with({
+      condition = with_root_file(local_eslint),
+    }),
+    formatting.eslint_d.with({
+      condition = with_root_file(local_eslint),
+    }),
+    formatting.shfmt,
+    formatting.stylua,
+    --formatting.google_java_format
+  },
+  on_attach = require("lsp.keymaps").on_attach,
 })
