062e4e47eb40a86657c6855561dc32a0e752c1c5
driden <driden@192.168.1.9>
2023-10-09 22:42:51 -0300

    fixed harpoon

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -9,7 +9,7 @@ nnoremap("h]", ":lua require(\"harpoon.ui\").nav_next()<CR>")
 nnoremap("ht", ":lua require(\"harpoon.term\").gotoTerminal(1)<CR>")
 
 nnoremap("<C-1>", ":lua require(\"harpoon.ui\").nav_file(1)<CR>")
-nnoremap("<C-2>", ":lua require(\"harpoon.ui\").nav_file(1)<CR>")
+nnoremap("<C-2>", ":lua require(\"harpoon.ui\").nav_file(2)<CR>")
 nnoremap("<C-3>", ":lua require(\"harpoon.ui\").nav_file(3)<CR>")
 
 require("telescope").load_extension('harpoon')
