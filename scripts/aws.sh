b66d9c619dae61bc9aa1d296ff2e8ef41f1651bf
Ignacio Loureiro <lrrezend@amazon.com>
2024-03-07 11:40:13 -0300

    cool new little script to select an aws profile

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -4,11 +4,16 @@ assume() {
 	local sess="sess_$(date +%s)"
 	local profile="${2:=default}"
 	export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-		$(aws sts assume-role \
+		$(
+			aws sts assume-role \
 				--role-arn "$1" \
 				--role-session-name "$sess" \
 				--profile "$profile" \
-			--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+				--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
 				--output text
 		))
+}
+
+select_role() {
+	rg --pcre2 -N -o -e '(?<=\[)\S+(?=\])' ~/.aws/credentials | fzf --no-multi --reverse --header 'Select a profile'
 }
