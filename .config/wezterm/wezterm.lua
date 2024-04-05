1fb63b62dfa45900b8670b08eda1b431492bc66d
Ignacio Loureiro <lrrezend@147dda3ab616.ant.amazon.com>
2024-04-05 10:19:51 -0300

    theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.color_scheme = 'Catppuccin Mocha'
 config.automatically_reload_config = true
 config.window_background_opacity = 0.7
 config.hide_tab_bar_if_only_one_tab = true
