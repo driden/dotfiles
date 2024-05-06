d3b148fcb29ac9cdc0f8cb209a30ad701d3e4219
Ignacio Loureiro <lrrezend@147dda3ab616.ant.amazon.com>
2024-05-06 11:23:44 -0300

    wip

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,14 +4,13 @@ local config = wezterm.config_builder()
 config.font_size = 14
 
 config.font = wezterm.font_with_fallback({
+  { family = "Hack" },
   { family = "Monaco" },
-  { family = "Courier Prime Code", weight = "Regular" },
-  { family = "Courier New",        weight = "Bold" },
-  { family = "Comic Code",         weight = "Medium" },
-  { family = "Hack Nerd Font",     weight = "Regular" },
-  { family = "Comic Mono",         weight = "Regular" },
-  { family = 'JetBrains Mono',     weight = 'Medium' },
-  { family = 'CodeNewRoman NF',    weight = 'Medium' },
+  { family = "Comic Code",      weight = "Medium" },
+  { family = "Hack Nerd Font",  weight = "Regular" },
+  { family = "Comic Mono",      weight = "Regular" },
+  { family = 'JetBrains Mono',  weight = 'Medium' },
+  { family = 'CodeNewRoman NF', weight = 'Medium' },
 })
 
 config.tab_max_width = 35
