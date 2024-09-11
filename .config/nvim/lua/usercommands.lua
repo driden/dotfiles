0f3a9f505544b92796819e204f0c7d9d05a08f35
Ignacio Loureiro <lrrezend@147dda3ab616.ant.amazon.com>
2024-09-11 10:19:16 -0300

    user commands

---
+++ .config/nvim/lua/usercommands.lua
@@ -0,0 +1,23 @@
+local commands = {
+  { name = 'Config', action = ':edit $HOME/.config/nvim/init.lua' },
+  { name = 'ReloadConfig', action = ':source ~/.config/nvim/init.lua' },
+  -- stylua: ignore start
+  { name = "RemoveEscapedQuotes", action = '%s#\\\\"#\"#g' },
+  -- stylua: ignore end
+  {
+    name = 'CopyPath',
+    action = function(data)
+      local file = vim.fn.expand '%:p'
+      vim.fn.setreg('+', file)
+      vim.notify('Copied file path to clipboard!', 2)
+    end,
+  },
+}
+
+local function create_command(name, action)
+  vim.api.nvim_create_user_command(name, action, {})
+end
+
+for _, value in ipairs(commands) do
+  create_command(value.name, value.action)
+end
