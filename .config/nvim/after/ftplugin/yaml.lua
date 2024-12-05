27e1694a022971f1df4d6591ebc1be21eeb4900e
Ignacio Loureiro <lrrezend@147dda3ab616.ant.amazon.com>
2024-12-05 11:36:42 -0300

    wip

--- .config/nvim/after/ftplugin/yaml.lua
+++ .config/nvim/after/ftplugin/yaml.lua
@@ -1,9 +1,11 @@
-vim.api.nvim_buf_create_user_command(0, "ConvertChecks", function()
+vim.api.nvim_buf_create_user_command(0, 'ConvertChecks', function()
   local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
   local old = vim.api.nvim_get_current_line()
   local replace = '\\1- keyCheck: $.\\2#\\1  valueCheck:\\3'
-  local search = "\\(\\s*\\)\\(\\p*\\):\\(.*\\)"
-  local lines = vim.split(vim.fn.substitute(old, search, replace, ""), "#")
+  local search = '\\(\\s*\\)\\(\\p*\\):\\(.*\\)'
+  local lines = vim.split(vim.fn.substitute(old, search, replace, ''), '#')
 
   vim.api.nvim_buf_set_lines(0, line - 1, line, false, lines)
 end, {})
+
+vim.opt_local.cursorcolumn = true
