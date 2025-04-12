b2f7632303e1414c140323b57e43f514852fd8bf
driden <driden@dridens-MacBook-Pro.local>
2025-04-12 16:36:24 -0300

    execute lua code by selecting it

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,2 +1,9 @@
--- 0 is current buffer
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>c", ":!lua %:t<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader><S-x>", ":luafile %<CR>", { desc = "execute: file", noremap = true, silent = true })
+
+-- help :{range}lua
+vim.keymap.set(
+  { "x", "v" },
+  "<leader>x",
+  "<cmd>'<,'>lua<CR>",
+  { desc = "execute: selected", noremap = true, silent = true, buffer = true }
+)
