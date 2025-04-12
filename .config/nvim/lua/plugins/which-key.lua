74d3b1ce350684389acad5ab28454e97f83a6701
driden <driden@dridens-MacBook-Pro.local>
2025-04-12 16:37:08 -0300

    moved which key to it's own file

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,61 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+  opts = {
+    icons = {
+      -- set icon mappings to true if you have a Nerd Font
+      mappings = vim.g.have_nerd_font,
+      -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+      -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+      keys = vim.g.have_nerd_font and {} or {
+        Up = "<Up> ",
+        Down = "<Down> ",
+        Left = "<Left> ",
+        Right = "<Right> ",
+        C = "<C-…> ",
+        M = "<M-…> ",
+        D = "<D-…> ",
+        S = "<S-…> ",
+        CR = "<CR> ",
+        Esc = "<Esc> ",
+        ScrollWheelDown = "<ScrollWheelDown> ",
+        ScrollWheelUp = "<ScrollWheelUp> ",
+        NL = "<NL> ",
+        BS = "<BS> ",
+        Space = "<Space> ",
+        Tab = "<Tab> ",
+        F1 = "<F1>",
+        F2 = "<F2>",
+        F3 = "<F3>",
+        F4 = "<F4>",
+        F5 = "<F5>",
+        F6 = "<F6>",
+        F7 = "<F7>",
+        F8 = "<F8>",
+        F9 = "<F9>",
+        F10 = "<F10>",
+        F11 = "<F11>",
+        F12 = "<F12>",
+      },
+    },
+    -- Document existing key chains
+    spec = {
+      { "<leader>c", group = "Code", mode = { "n", "x" } },
+      { "<leader>b", group = "Buffer", mode = { "n", "x" } },
+      { "<leader>d", group = "Debug" },
+      { "<leader>g", group = "Go" },
+      { "<leader>f", group = "Find" },
+      { "<leader>t", group = "Toggle" },
+      { "<leader>q", group = "Quickfix" },
+    },
+  },
+}
