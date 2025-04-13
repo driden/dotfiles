663e9b4bcd4804f221457b7ace31de0c6863fd10
ddn <mail@mail.com>
2025-04-13 12:23:49 -0300

    Obsidian notes folder update
    Obsidian can now take a list of folders and search for obsidian workspaces in them, so we can discover where the actual files are instead of just hardcoding it

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,24 +1,29 @@
-local notes_dir = "~/Documents/Notes"
-local notes_expanded = vim.fn.expand(notes_dir)
-local workspaces = {}
-for dir, type in vim.fs.dir(notes_expanded) do
-  if type == "directory" then
-    -- https://www.lua.org/manual/5.1/manual.html
-    -- 5.4.1 patterns
-    -- filter out hidden folders
-    if not dir:match("%p") then
-      local candidate = notes_expanded .. "/" .. dir
-      -- if candidate has an `.obsidian` folder then we can add it
-      if vim.uv.fs_stat(candidate .. "/.obsidian") then
-        local name = candidate:match("([^/]+)$")
-        table.insert(workspaces, { name = name, path = candidate })
+local notes_dirs = { "~/Documents/Notes", "~/notes" }
+
+local workspaces = vim
+  .iter(notes_dirs)
+  :map(function(dir)
+    return vim.fn.expand(dir)
+  end)
+  :map(function(curr_dir)
+    for dir, type in vim.fs.dir(curr_dir) do
+      if type == "directory" then
+        -- https://www.lua.org/manual/5.1/manual.html
+        -- 5.4.1 patterns
+        -- filter out hidden folders
+        if not dir:match("%p") then
+          local candidate = curr_dir .. "/" .. dir
+          -- if candidate has an `.obsidian` folder then we can add it
+          if vim.uv.fs_stat(candidate .. "/.obsidian") then
+            local name = candidate:match("([^/]+)$")
+            return { name = name, path = candidate }
+          end
+        end
       end
     end
-  end
-end
+  end)
+  :totable()
 
-local personal_vault = vim.fn.expand("~/Documents/Notes/Markdown")
-local work_vault = vim.fn.expand("~/Documents/Notes/work")
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
