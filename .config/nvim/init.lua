02f04ba5d8485e90b7e6d816573b11a7f21e84de
driden <driden@dridens-MacBook-Pro.local>
2025-10-11 11:43:22 -0300

    ruff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -165,7 +165,13 @@ require("lazy").setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, java = true, typecript = true }
+        local disable_filetypes = {
+          c = true,
+          cpp = true,
+          java = true,
+          typecript = true,
+          python = true,
+        }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = "never"
@@ -180,7 +186,7 @@ require("lazy").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "ruff_format" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
