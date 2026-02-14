d8102f664d6bc669b4d419e678576cbe2603f49c
driden <driden@dridens-MacBook-Pro.local>
2026-02-14 12:50:48 -0300

    updated fish with fzf themes and commands i use on zsh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,11 +62,23 @@ alias twl="terraform workspace list"
 alias twc="terraform workspace list | grep '*' | tr -d '*'| tr -d '[:space:]'"
 
 fzf --fish | source
-# zoxide init fish | source
+
+set -gx FZF_DEFAULT_COMMAND 'rg --hidden -l ""'
+set -gx FZF_ALT_C_COMMAND 'fd --absolute-path --type d --max-depth 2 -E Library -E Pictures -E Music -E Applications -E zsh-plugins -E go . "$HOME"'
+
+# FZF Theme Configuration
+if test "$THEME" = "catppuccin"
+    set -gx FZF_DEFAULT_OPTS "--layout=reverse --border=rounded --color=bg+:#313244,bg:#1e1e2e,preview-bg:#181825,fg:#cdd6f4,fg+:#cdd6f4,hl:#f38ba8,hl+:#f38ba8,info:#cba6f7,prompt:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,spinner:#f5e0dc,header:#94e2d5,border:#b4befe"
+else
+    set -gx FZF_DEFAULT_OPTS "--layout=reverse --border=rounded --color=bg+:#383b35,bg:#252623,preview-bg:#1c1e1b,fg:#f1e9d2,fg+:#f1e9d2,hl:#e75a7c,hl+:#e75a7c,info:#aaaaff,prompt:#8fb573,pointer:#dbb651,marker:#70c2be,spinner:#57a5e5,header:#8fb573,border:#8fb573"
+end
+
+set -gx FZF_ALT_C_OPTS "--preview 'eza --icons --color=always --tree --level=1 {}' --preview-window=right:50%:wrap"
+
 mise activate fish | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting # disable welcome message
-    # starship init fish | source
+    starship init fish | source
 end
