7cb186f7d4a68dbc4998114040e139bd40ae8995
driden <lloureiro@rccl.com>
2026-03-31 10:28:17 -0300

    disabled avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,6 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  enabled = false,
   version = false, -- Never set this value to "*"! Never!
   config = function()
     require("avante").setup({
