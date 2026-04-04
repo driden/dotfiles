578528eeee6b236a6e2d42442e3ee04e961c4baa
driden <driden@dridens-MacBook-Pro.local>
2026-04-04 20:32:04 -0300

    things

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -174,7 +174,6 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        "stylua", -- Used to format Lua code
         "kotlin-lsp", -- v0.252.17811
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
@@ -236,6 +235,8 @@ return {
       --     -- end,
       --   },
       -- })
+      --
+      require("driden.lsp.html").setup(vim.deepcopy(capabilities))
 
       -- bash
       vim.lsp.config("bashls", {
