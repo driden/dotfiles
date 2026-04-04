600e5e7193fb9e51322af737d090680996dfbdeb
driden <driden@dridens-MacBook-Pro.local>
2026-04-04 20:32:04 -0300

    things

---
+++ .config/nvim/lua/driden/lsp/html.lua
@@ -0,0 +1,56 @@
+local M = {}
+
+function M.setup(capabilities)
+  ---@brief
+  ---
+  --- https://github.com/hrsh7th/vscode-langservers-extracted
+  ---
+  --- `vscode-html-language-server` can be installed via `npm`:
+  --- ```sh
+  --- npm i -g vscode-langservers-extracted
+  --- ```
+  ---
+  --- Neovim does not currently include built-in snippets. `vscode-html-language-server` only provides completions when snippet support is enabled.
+  --- To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
+  ---
+  --- The code-formatting feature of the lsp can be controlled with the `provideFormatter` option.
+  ---
+  --- ```lua
+  --- --Enable (broadcasting) snippet capability for completion
+  --- local capabilities = vim.lsp.protocol.make_client_capabilities()
+  --- capabilities.textDocument.completion.completionItem.snippetSupport = true
+  ---
+  --- vim.lsp.config('html', {
+  ---   capabilities = capabilities,
+  --- })
+  --- ```
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  vim.lsp.config("html", {
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html" },
+    root_markers = { "package.json", ".git" },
+    ---@type lspconfig.settings.html
+    settings = {
+      html = {
+        settings = {
+          css = {
+            lint = {
+              validProperties = {},
+            },
+          },
+        },
+        format = {},
+      },
+    },
+    init_options = {
+      provideFormatter = true,
+      embeddedLanguages = { css = true, javascript = true },
+      configurationSection = { "html", "css", "javascript" },
+    },
+  })
+
+  vim.lsp.enable("html")
+end
+
+return M
