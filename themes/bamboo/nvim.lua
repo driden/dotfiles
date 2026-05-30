d06b99dca8bb8ef59183cea71101964c9fb601e3
driden <driden@dridens-MacBook-Pro.local>
2026-05-30 19:39:45 -0300

    theming: scaffold bamboo theme directory

---
+++ themes/bamboo/nvim.lua
@@ -0,0 +1,7 @@
+-- themes/bamboo/nvim.lua
+-- Selects which nvim plugin/variant `driden.theme.apply()` loads for this theme.
+return {
+  plugin = "bamboo.nvim",
+  colorscheme = "bamboo",
+  setup = function() end,
+}
