506de89091af51aafdf102db332eae650fba4ed1
driden <driden@dridens-MacBook-Pro.local>
2026-06-06 14:56:00 -0300

    themes

--- themes/bamboo/nvim.lua
+++
@@ -1,7 +0,0 @@
--- themes/bamboo/nvim.lua
--- Selects which nvim plugin/variant `driden.theme.apply()` loads for this theme.
-return {
-  plugin = "bamboo.nvim",
-  colorscheme = "bamboo",
-  setup = function() end,
-}
